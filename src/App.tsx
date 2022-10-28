ac3ba0d8803591c9577fa7f22eaa21e7e92dc5c1
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-27 22:29:08 -0300

    Page Movies ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./assets/custom.scss";
 import "./App.css";
 import Login from "./pages/Login";
 import NavBar from "./components/NavBar";
+import Movies from "./pages/Movies";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Login />
+      {/* <Login /> */}
+      <Movies />
     </>
   );
 }
---
+++ src/pages/Movies/index.tsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const Movies = () => {
+  return (
+    <div className="movies-list">
+      <h1>Tela listagem de filmes</h1>
+      <a className="mt-2" href="#">
+        Acessar /movies/1
+      </a>
+      <a className="mt-2" href="#">
+        Acessar /movies/2
+      </a>
+    </div>
+  );
+};
+
+export default Movies;
