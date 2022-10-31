3c6bf86819e0118f41550c4e8badefec4a78d785
Erick Oliveira <erick.oliveira9607@gmail.com>
2022-10-31 17:40:29 -0300

    implementando login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,21 @@
-import Button from "../../components/BasicButton";
 import ImgLogin from "../../assets/img/login-img.svg";
 
 import "./styles.css";
 import BasicButton from "../../components/BasicButton";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  username: string;
+  password: string;
+};
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData);
+  };
+
   return (
     <div className="login-container">
       <div className="login-content mt-4">
@@ -17,9 +28,10 @@ const Login = () => {
       <div className="login-auth base-card">
         <h1 className="mb-5 mt-5">LOGIN</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-3">
             <input
+              {...register("username")}
               type="text"
               className="form-control input-style"
               placeholder="Email"
@@ -29,6 +41,7 @@ const Login = () => {
 
           <div className="mb-5">
             <input
+              {...register("password")}
               type="password"
               className="form-control input-style"
               placeholder="Senha"
